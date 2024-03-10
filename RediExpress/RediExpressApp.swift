ae1a54ecb51a9ae975984ee82b5adbc87f2cd8c2
smetankaIa <alex.gg3200@gmail.com>
2024-03-10 18:21:31 +0700

    Initial Commit

---
+++ RediExpress/RediExpressApp.swift
@@ -0,0 +1,17 @@
+//
+//  RediExpressApp.swift
+//  RediExpress
+//
+//  Created by Алексей Никулин on 10.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct RediExpressApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
